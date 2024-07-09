e6e04b6c6bbdf8b0db2e564debc9e5400de7accd
Karen Mui <karen_mui@hotmail.com>
2024-07-08 20:10:31 -0400

    finished recursive practice

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let num = fromN;
+
+  function add(fromN, toN) {
+
+    if (fromN + 1 <= toN) {
+      num += (fromN + 1)
+      add(fromN + 1, toN)
+    }
+  }
+
+  add(fromN, toN)
+
+
+  return num;
+
 }
+
+console.log(sum(3, 7))
 
 module.exports = sum;
